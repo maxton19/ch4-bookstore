7d790367ff1d0d3239098b8343eb76ef53a1aab2
rian19 <intendcourse@gmail.com>
2023-08-06 03:12:42 +0700

    ch5

--- accounts/tests.py
+++ accounts/tests.py
@@ -6,10 +6,10 @@ class CustomUserTests(TestCase):
     def test_create_user(self):
         User = get_user_model()
         user = User.objects.create_user(
-            username="will", email="will@email.com", password="testpass123"
+            username="RianAji", email="m.syahrian.aji@gmail.com", password="Rian1907"
         )
-        self.assertEqual(user.username, "will")
-        self.assertEqual(user.email, "will@email.com")
+        self.assertEqual(user.username, "RianAji")
+        self.assertEqual(user.email, "m.syahrian.aji@gmail.com")
         self.assertTrue(user.is_active)
         self.assertFalse(user.is_staff)
         self.assertFalse(user.is_superuser)
