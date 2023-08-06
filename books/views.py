2fb942668a87342da167c830d7207b240923bda2
rian19 <intendcourse@gmail.com>
2023-08-07 05:43:00 +0700

    ch15

--- books/views.py
+++ books/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth.mixins import (
     LoginRequiredMixin,
     PermissionRequiredMixin  # new
 )
+from django.db.models import Q  # new
 from django.views.generic import ListView, DetailView
 
 from .models import Book
@@ -23,3 +24,20 @@ class BookDetailView(
     template_name = "books/book_detail.html"
     login_url = "account_login"
     permission_required = "books.special_status"  # new
+
+
+class SearchResultsListView(ListView):  # new
+    model = Book
+    context_object_name = "book_list"
+    template_name = "books/search_results.html"
+    queryset = Book.objects.filter(title__icontains="beginners") # new
+    
+    def get_queryset(self): # new
+        query = self.request.GET.get("q")
+        return Book.objects.filter(
+            Q(title__icontains=query) | Q(author__icontains=query)
+        )
+
+
+    
+    
